51bf7f117ca2699a20503162a5ed9e40c84b764c
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-07-07 14:01:21 +0200

    saludo lo que sea

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hola amigos')
+});
 app.use('/api', require('./routes/api'));
 
 module.exports = app;
